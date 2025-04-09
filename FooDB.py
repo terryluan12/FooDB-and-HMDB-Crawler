ef91fdbbb09066b9a083e2e6005a9f155f8b4dde
Terry <terryluan12@gmail.com>
2025-04-09 01:42:38 -0400

    Added checks in FooDB and fixed scrawling

--- FooDB.py
+++ FooDB.py
@@ -15,37 +15,70 @@ def parseWebpage():
         rows = soup.find_all("a", class_="btn-show")
         ids = [link.text for link in rows]
 
-    for id in ids:
-        page = requests.get(MET_PAGE + id)
-        soup = bs(page.text, features="xml")
+        print(f"DEBUG: Got ID's for page {page_num}")
+        print(f"----------------------------------------------------------")
+    
+        for id in ids:
+            print(f"DEBUG: Working with id: {id}")
+            page = requests.get(MET_PAGE + id)
+            soup = bs(page.text, features="xml")
 
-        fooDB_name = getName(soup)
-        met_class = getClass(soup)
-        food_map = getAssociatedFoods(soup)
+            fooDB_name = getName(soup)
+            met_class = getClass(soup)
+            food_map = getAssociatedFoods(soup)
 
-        print(f"""
-    Information for {id}:
-    fooDB Name: {fooDB_name}
-    Class: {met_class}
-    Associated Foods: {food_map}
-            """)
+        #     print(f"""
+        # Information for {id}:
+        # fooDB Name: {fooDB_name}
+        # Class: {met_class}
+        # Associated Foods: {food_map}
+        #         """)
         
     
 def getName(soup):
-    return soup.find("name").text
+    name = soup.find("name")
+    if not name or not name.string:
+        print("\tAlert: No Name")
+        return
+    return name.string
 
 def getClass(soup):
-    return soup.find("class").text
+    met_class = soup.find("class")
+    if not met_class or not met_class.string:
+        print("\tAlert: No Class")
+        return
+    return met_class.string
 
 def getAssociatedFoods(soup):
     foods = {}
-    food_table = soup.find_all("foods")
-    for row in food_table:
-        name = row.find("name").text
+    food_table = soup.find("foods")
+    for row in food_table.find_all("food"):
+        name_tag = row.find("name")
+        if not name_tag or not name_tag.string:
+            print("\tAlert: No Food Name")
+            continue
+        name = name_tag.string
         foods[name] = {}
-        foods[name]["average_value"] = row.find("average_value").text
-        foods[name]["max_value"] = row.find("max_value").text
-        foods[name]["min_value"] = row.find("min_value").text
+        average_value = row.find("average_value")
+        if not average_value or not average_value.string:
+            print("\tAlert: No Average value")
+        else:
+            foods[name]["average_value"] = average_value.string
+            
+        max_value = row.find("max_value")
+        if not max_value or not max_value.string:
+            print("\tAlert: No Max value")
+        else:
+            foods[name]["max_value"] = max_value.string
+            
+        min_value = row.find("min_value")
+        if not min_value or not min_value.string:
+            print("\tAlert: No Min value")
+        else:
+            foods[name]["min_value"] = min_value.string
+        if foods[name]["min_value"] == '0.0' and foods[name]["max_value"] == '0.0':
+            del foods[name]
+        
     return foods
 
 if __name__ == "__main__":
--- HMDB.py
+++ HMDB.py
@@ -16,43 +16,44 @@ def parseWebpage():
         print(f"DEBUG: Got ID's for page {page_num}")
         print(f"----------------------------------------------------------")
 
-    for id in ids:
-        print(f"DEBUG: Working with id: {id}")
-        page = requests.get(MET_PAGE + id + ".xml")
-        soup = bs(page.text, features="xml")
+        for id in ids:
+            print(f"DEBUG: Working with id: {id}")
+            page = requests.get(MET_PAGE + id + ".xml")
+            soup = bs(page.text, features="xml")
 
-        common_name = getName(soup)
-        loci = getLoci(soup)
-        food_map = getFoodSources(soup)
-        concentrations = getConcentrations(soup, True)
-        abconcentrations = getConcentrations(soup, False)
-        
-        print(f"""
-    Information for {id}:\n
-    Common Name: {common_name}\n
-    Biospecimen Location: {loci}\n
-    Food Source: {food_map}\n
-    Concentrations: {concentrations}\n
-    AbConentrations: {abconcentrations}\n
-            """)
-        
+            common_name = getName(soup)
+            loci = getLoci(soup)
+            food_map = getFoodSources(soup)
+            concentrations = getConcentrations(soup, True)
+            abconcentrations = getConcentrations(soup, False)
+            
+            print(f"""
+        Information for {id}:\n
+        Common Name: {common_name}\n
+        Biospecimen Location: {loci}\n
+        Food Source: {food_map}\n
+        Concentrations: {concentrations}\n
+        AbConentrations: {abconcentrations}\n
+                """)
     
 def getName(soup):
     name = soup.find("name")
-    if not name:
+    if not name or not name.string:
         print("\tAlert: No Name")
-    return name.text
+        return
+    return name.string
 
 def getLoci(soup):
     location_tags = soup.find("biospecimen_locations")
     if not location_tags:
         print("\tAlert: No Biospecimen locations")
+        return
     locations = [loc.text for loc in location_tags.find_all("biospecimen")]
     return locations
 
 def getFoodSources(soup):
     foodb_id = soup.find("foodb_id")
-    if not foodb_id or foodb_id.string:
+    if not foodb_id or not foodb_id.string:
         print("\tAlert: No Food ID")
         return
     page = requests.get(MET_FOODB_PAGE + foodb_id.string)
@@ -61,40 +62,46 @@ def getFoodSources(soup):
     food_tags = foodb_soup.find("foods")
     foods = []
     if not food_tags:
-        print("\tError: No food data")
+        print("\tAlert: No food data")
+        return
         
     for food in food_tags.find_all("food"):
         foods.append(food.find("name").string)
     return foods
 
 def getConcentrations(soup, normal = True):
-    # Details header ignored
-    headers = ["biospecimen", "Status", "Value", "Age", "Sex", "Condition", "Reference"]
-
-    def meetsCriteria(tag):
-        """Currently looks for only in blood and that it can be quantified
-
-        Args:
-            tag (bs4.element.tag): the tag being filtered
-        """
-        return tag.name == "td" and tag.text == "Detected and Quantified"
     conc_table = soup.find("normal_concentrations") if normal else soup.find("abnormal_concentrations")
     
     if not conc_table:
         print(f"\tAlert: {'Normal Concentrations' if normal else 'Abnormal Concentrations'} missing")
+        return
     concs = conc_table.find_all("concentration")
     if not concs:
         print(f"\tError: Concentrations missing")
+        return
         
     concentrations = []
+    col_map = {
+        "subject_age": "age",
+        "patient_age": "age",
+        "subject_sex": "sex",
+        "patient_sex": "sex",
+        "subject_condition": "condition",
+        "patient_information": "condition",
+    }
     
     for conc in concs:
         concentration = {}
-        for col in conc.find_all(True):
-            if col.name == "references":
+        isQuantified = True
+        print(f"Conc.findAll is {conc.find_all(True)}")
+        for tag in conc.find_all(True):
+            name = tag.name
+            if name == "references":
                 concentration["references"] = []
-                references = col.find_all("reference")
-                if len(references) != 1:
+                references = tag.find_all("reference")
+                if len(references) == 0:
+                    print(f"\tAlert: Zero references")
+                elif len(references) != 1:
                     print(f"\tAlert: More than one reference")
                 for reference in references:
                     ref = {}
@@ -102,8 +109,17 @@ def getConcentrations(soup, normal = True):
                         ref[ref_col.name] = ref_col.text
                     concentration["references"].append(ref)
             else:
-                concentration[col.name] = col.text
-        concentrations.append(concentration)
+                if name == "reference":
+                    continue
+                if tag.text == "Not Specified" or tag.text == "Not Quantified" or not tag.string:
+                    print(f"It is {tag}")
+                    isQuantified = False
+                    break
+                if name in col_map:
+                    name = col_map[name]
+                concentration[name] = tag.string
+        if isQuantified:
+            concentrations.append(concentration)
     return concentrations
 
 if __name__ == "__main__":
